b9be53454d111375a722de0a39a6c24b68ef274a
vani88-git <bhatvani8884@gmail.com>
2025-11-06 23:26:39 +0530

    Added Logo and Navigation bar to the App.js and added Meta tags to index.html

---
+++ little-lemon-reservation/src/Nav.js
@@ -0,0 +1,16 @@
+function Nav()
+{
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/menu">Menu</a></li>
+        <li><a href="/reser nve">Reserve</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+export default Nav;
+
+
